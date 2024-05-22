1a1717e6efcc5680f8e16b4afb05494ccda4b2c3
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-05-22 19:09:03 -0300

    create porId method

--- src/main/java/org/taba/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/taba/java/jdbc/EjemploJdbc.java
@@ -12,7 +12,9 @@ public class EjemploJdbc {
     public static void main(String[] args) {
         try (Connection conn = ConexionBaseDatos.getInstance()){
             Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
-            repositorio.listar().forEach(p -> System.out.println(p.getNombre()));
+            repositorio.listar().forEach(System.out::println);
+
+            System.out.println(repositorio.porId(1L));
 
         } catch (SQLException e) {
             e.printStackTrace();
