a047e3ff965d24a4e31fad0824e9726be63dc308
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-05-22 19:01:56 -0300

    create porId method

--- src/main/java/org/taba/java/jdbc/repository/ProductoRepositorioImpl.java
+++ src/main/java/org/taba/java/jdbc/repository/ProductoRepositorioImpl.java
@@ -17,17 +17,22 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
         try(Statement stmt = getConnection().createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM productos")) {
             while(rs.next()){
-                Producto p = new Producto();
-                p.setId(rs.getLong("id"));
-                p.setNombre(rs.getString("nombre"));
-                p.setPrecio(rs.getInt("precio"));
-                p.setFechaRegistro(rs.getDate("fecha_registro"));
+                Producto p = crearProducto(rs);
                 productos.add(p);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return productos;
+    }
+
+    private static Producto crearProducto(ResultSet rs) throws SQLException {
+        Producto p = new Producto();
+        p.setId(rs.getLong("id"));
+        p.setNombre(rs.getString("nombre"));
+        p.setPrecio(rs.getInt("precio"));
+        p.setFechaRegistro(rs.getDate("fecha_registro"));
+        return p;
     }
 
     @Override
@@ -37,11 +42,14 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
                 prepareStatement("SELECT * FROM productos WHERE id = ?")){
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
-
+            if (rs.next()){
+                producto = crearProducto(rs);
+            }
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return producto;
     }
 
     @Override
